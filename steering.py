f74e85bd7afb4eec296eef62dad7d6197296d9f4
Banana4050 <master-ellermann@gmx.de>
2022-05-25 11:09:58 +0200

    von Lars

--- steering.py
+++ steering.py
@@ -1,9 +1,37 @@
-def steering_angle(line_angle):
-    """Umrechung Lenkwinkel"""
-    steering_angle_car = line_angle*10
+import basisklassen_cam
+import basecar as bc
+import preprocess_frame as pf
+import compute_lines as cl
+import cv2 as cv
 
-    return steering_angle_car
+def steering_angle(line_angle):
+    """
+    Umrechung Lenkwinkel
+    line_angle liegt zw. 45 und 135
+    line_angle = 360 ist ungültig
+    """
+
+    if line_angle != 360: 
+        steering_angle_car = round(line_angle, 1)
+        return steering_angle_car
+    else:
+        return 360
 
 if __name__ == "__main__":
-    import CamCar as car
+    cam = basisklassen_cam.Camera()
+    car = bc.BaseCar()
+
+    testbild = cam.get_frame()
+    testbild = pf.preprocess_frame(testbild, 0.5, 255, 255) #Parameter ggf. anpassen
+    img, line_angle = cl.get_lines(testbild)
+
+    cv.imshow("Testbild", img)
     print("TESTUMGEBUNG")
+    print("Linienwinkel = ", line_angle)
+    print("Lenkwinkel = ", steering_angle(line_angle))
+    print()
+
+    car.steering_angle = steering_angle(line_angle)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
